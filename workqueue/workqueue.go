f9a6cfef74112c887b38225fdbfaf8d8d603200b
YaoZengzeng <yaozengzeng@zju.edu.cn>
2018-05-14 16:09:40 +0800

    work queue

---
+++ workqueue/workqueue.go
@@ -0,0 +1,121 @@
+package workqueue
+
+import (
+	"sync"
+)
+
+type Interface interface {
+	Add(item interface{})
+	Len() int
+	Get() (item interface{}, shutdown bool)
+	Done(item interface{})
+	ShutDown()
+	ShuttingDown() bool
+}
+
+type empty struct{}
+type t interface{}
+type set map[t]empty
+
+func (s set) insert(k interface{}) {
+	s[k] = empty{}
+}
+
+func (s set) delete(k interface{}) {
+	delete(s, k)
+}
+
+func (s set) has(k interface{}) bool {
+	_, ok := s[k]
+	return ok
+}
+
+type Type struct {
+	queue	[]t
+
+	dirty	set
+	processing set
+
+	shuttingDown bool
+
+	cond	*sync.Cond	
+}
+
+func New() *Type {
+	return &Type{
+		dirty:		make(set),
+		processing:	make(set),
+		cond:		sync.NewCond(&sync.Mutex{}),
+	}
+}
+
+func (t *Type) Add(item interface{}) {
+	t.cond.L.Lock()
+	defer t.cond.L.Unlock()
+
+	if t.shuttingDown {
+		return
+	}
+
+	if t.dirty.has(item) {
+		return
+	}
+
+	t.dirty.insert(item)
+
+	if !t.processing.has(item) {
+		t.queue = append(t.queue, item)
+	}
+
+	t.cond.Signal()
+}
+
+func (t *Type) Get() (interface{}, bool) {
+	t.cond.L.Lock()
+	defer t.cond.L.Unlock()
+
+	for len(t.queue) == 0 && t.shuttingDown != true {
+		t.cond.Wait()
+	}
+
+	if len(t.queue) == 0 {
+		return nil, true
+	}
+
+	var item interface{}
+
+	item, t.queue = t.queue[0], t.queue[1:]
+	t.dirty.delete(item)
+	t.processing.insert(item)
+	return item, false
+}
+
+func (t *Type) Done(item interface{}) {
+	t.cond.L.Lock()
+	defer t.cond.L.Unlock()
+
+	t.processing.delete(item)
+	if t.dirty.has(item) {
+		t.queue = append(t.queue, item)
+	}
+}
+
+func (t *Type) ShutDown() {
+	t.cond.L.Lock()
+	defer t.cond.L.Unlock()
+	t.shuttingDown = true
+	t.cond.Broadcast()
+}
+
+func (t *Type) Len() int {
+	t.cond.L.Lock()
+	defer t.cond.L.Unlock()
+	return len(t.queue)
+}
+
+func (t *Type) ShuttingDown() bool {
+	t.cond.L.Lock()
+	defer t.cond.L.Unlock()
+	return t.shuttingDown
+}
+
---
+++ workqueue/workqueue_test.go
@@ -0,0 +1,93 @@
+package workqueue
+
+import (
+	"testing"
+	"sync"
+	"time"
+)
+
+func TestBasic(t *testing.T) {
+	q := New()
+
+	const producer = 50
+	producerWG := sync.WaitGroup{}
+	producerWG.Add(producer)
+	for i := 0; i < producer; i++ {
+		go func(i int) {
+			defer producerWG.Done()
+			for j := 0; j < 50; j++ {
+				q.Add(i)
+				time.Sleep(time.Millisecond)
+			}
+		}(i)
+	}
+
+	const consumer = 10
+	consumerWG := sync.WaitGroup{}
+	consumerWG.Add(consumer)
+	for i := 0; i < consumer; i++ {
+		go func(i int) {
+			defer consumerWG.Done()
+			for {
+				item, quit := q.Get()
+				if item == "added after shutdown!" {
+					t.Errorf("Got an item added after shutdown")
+				}
+				if quit {
+					break
+				}
+				t.Logf("Worker %v: begin processing %v", i, item)
+				time.Sleep(3 * time.Millisecond)
+				t.Logf("Worker %v: done processing %v", i, item)
+				q.Done(item)
+			}
+		}(i)
+	}
+
+	producerWG.Wait()
+	q.ShutDown()
+	q.Add("added after shutdown!")
+	consumerWG.Wait()
+}
+
+func TestLen(t *testing.T) {
+	q := New()
+	q.Add("foo")
+	if e, a := 1, q.Len(); e != a {
+		t.Errorf("Expected %v, got %v", e, a)
+	}
+	q.Add("bar")
+	if e, a := 2, q.Len(); e != a {
+		t.Errorf("Expected %v, got %v", e, a)
+	}
+	q.Add("foo")
+	if e, a := 2, q.Len(); e != a {
+		t.Errorf("Expected %v, got %v", e, a)
+	}
+}
+
+func TestReinsert(t *testing.T) {
+	q := New()
+
+	q.Add("foo")
+
+	i, _ := q.Get()
+	if i != "foo" {
+		t.Errorf("Expected %v, got %v", "foo", i)
+	}
+
+	q.Add(i)
+
+	q.Done(i)
+
+	i, _ = q.Get()
+	if i != "foo" {
+		t.Errorf("Expected %v, got %v", "foo", i)
+	}
+
+	q.Done(i)
+
+	if a := q.Len(); a != 0 {
+		t.Errorf("Expected queue to be empty. Has %v items", a)
+	}
+}
