1af78b6733afd7b43b792b88b8ab0b71a67b6741
YaoZengzeng <yaozengzeng@zju.edu.cn>
2018-05-15 14:31:44 +0800

    priority queue

---
+++ priorityqueue/priorityqueue_test.go
@@ -0,0 +1,56 @@
+package priorityqueue
+
+import (
+	"testing"
+	"container/heap"
+)
+
+func TestPriorityQueue(t *testing.T) {
+	m := map[string]int{
+		"abc":3,
+		"def":1,
+		"ghi":10,
+	}
+
+	var q PriorityQueue
+	index := 0
+	for value, priority := range m {
+		q = append(q, &Item{
+			value:		value,
+			priority:	priority,
+			index:		index,
+		})
+		index++
+	}
+
+	heap.Init(&q)
+
+	item := &Item{
+		value:		"jkl",
+		priority:	5,
+	}
+
+	heap.Push(&q, item)
+
+	q.Update(item, "xyz", 20)
+
+	cases := []struct{
+		value 		string
+		priority 	int
+	}{
+		{"xyz", 20},
+		{"ghi", 10},
+		{"abc", 3},
+		{"def", 1},
+	}
+	for _, c := range cases {
+		item = heap.Pop(&q).(*Item)
+		if c.value != item.value || c.priority != item.priority {
+			t.Fatalf("Unexpected item, expected: %s/%d, real: %s/%d", c.value, c.priority, item.value, item.priority)
+		}
+	}
+
+	if len(q) != 0 {
+		t.Fatalf("PriorityQueue should be empty finally")
+	}
+}
